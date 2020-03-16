9d8945ff350966567aab948a280d77da0f353cff
Jirka <jiri.moucka@idealine.cz>
2020-03-16 12:49:25 +0100

    nový řádek 1

---
+++ ConsoleApplicationPokus_2/ConsoleApplicationPokus_2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApplicationPokus_2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("ahoj");
+        }
+    }
+}
